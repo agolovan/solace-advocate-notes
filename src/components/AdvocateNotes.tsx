c7b95889c1b856db3af96e9b236b237155ab8638
Alexander Golovan <a_golovan@hotmail.com>
2024-03-16 01:39:36 -0700

    adding schema

--- src/components/AdvocateNotes.tsx
+++ src/components/AdvocateNotes.tsx
@@ -1,12 +1,26 @@
-/* eslint react/prop-types: 0 */
-import React from "react";
+import React, { useEffect } from "react";
 import { AdminView } from "payload/config";
 import { DefaultTemplate } from "payload/components/templates";
 
 import "./Components.scss";
 
+// eslint-disable-next-line react/prop-types
 const AdvocateNotes: AdminView = ({ user }) => {
   console.log(user);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      // const { userDefinedModels, errorMessage } =
+      //   await fetchModelsClientUnique();
+      // setModels(userDefinedModels);
+      // if (errorMessage) {
+      //   toast.error(errorMessage);
+      // }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <DefaultTemplate>
       <div className="modelsContainer">
